7690a3a0535cf939614c8b50a6bd07d64a022886
Shaheer <shaheerm@hexaware.com>
2025-05-21 13:39:10 +0200

    initial project set up

---
+++ src/main/kotlin/com/full/stack/assignment/f1/F1Application.kt
@@ -0,0 +1,11 @@
+package com.full.stack.assignment.f1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class F1Application
+
+fun main(args: Array<String>) {
+	runApplication<F1Application>(*args)
+}
---
+++ src/test/kotlin/com/full/stack/assignment/f1/F1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.full.stack.assignment.f1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class F1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
