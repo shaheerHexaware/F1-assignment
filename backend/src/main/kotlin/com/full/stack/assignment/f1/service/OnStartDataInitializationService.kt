cc74993701bdbe24a7326ebbcbd27e5d10e0a708
Shaheer <shaheerm@hexaware.com>
2025-05-30 21:17:38 +0200

    Rectified access to Controller from service by switching to using F1Service instead of F1Controller. Added logging and tests for Initialization service

--- backend/src/main/kotlin/com/full/stack/assignment/f1/service/OnStartDataInitializationService.kt
+++ backend/src/main/kotlin/com/full/stack/assignment/f1/service/OnStartDataInitializationService.kt
@@ -1,20 +1,25 @@
 package com.full.stack.assignment.f1.service
 
-import com.full.stack.assignment.f1.controller.F1Controller
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class OnStartDataInitializationService(
-    private val f1Controller: F1Controller,
+    private val f1Service: F1Service,
     @Value("\${data.initializer.start.year}") private val startYear: Int,
     @Value("\${data.initializer.end.year}") private val endYear: Int? = null,
 ) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     fun initializeF1Data() {
         try {
-            f1Controller.getSeasonsWithChampions(startYear, endYear)
+            val endYear = endYear ?: java.time.Year.now().value
+
+            logger.info("Initializing data for the period $startYear to $endYear")
+            f1Service.getSeasons(startYear, endYear)
         } catch (e: Exception) {
-            println("Failed to initialize F1 data: $e")
+            logger.error("Error while initializing data: $e")
         }
     }
 }
