395368b1588a3b283a701442b4d09b7e1af641b1
Shaheer <shaheerm@hexaware.com>
2025-06-02 14:57:45 +0200

    removed unused import from Controller

--- backend/src/main/kotlin/com/full/stack/assignment/f1/controller/F1Controller.kt
+++ backend/src/main/kotlin/com/full/stack/assignment/f1/controller/F1Controller.kt
@@ -6,7 +6,6 @@ import com.full.stack.assignment.f1.model.Race
 import com.full.stack.assignment.f1.model.Season
 import com.full.stack.assignment.f1.service.F1Service
 import jakarta.validation.constraints.Min
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
